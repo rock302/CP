59cd898e821935154745f4334a2ee3b295ae0dc6
Aditya <adidada023@gmail.com>
2022-11-20 15:35:27 +0530

    added

--- Assignment 7/Interprocess Communication/Shared Memory/reader.c
+++ Assignment 7/Interprocess Communication/Shared Memory/reader.c
@@ -1,9 +1,8 @@
-#include <iostream>
 #include <sys/ipc.h>
 #include <sys/shm.h>
 #include <stdio.h>
-using namespace std;
-main() {
+
+int main() {
    key_t my_key = ftok("shmfile",65); // ftok function is used to generate unique key
    int shmid = shmget(my_key,1024,0666|IPC_CREAT); // shmget returns an ide in shmid
    char *str = (char*) shmat(shmid,(void*)0,0); // shmat to join to shared memory
